cd19c3273ff0a8eba1b8c0f3a91230147abe2809
bvpk918 <jin33888829@gamil.com>
2023-04-22 02:18:24 +0900

    얼추다됐나

--- pp/js/maman.js
+++ pp/js/maman.js
@@ -38,11 +38,69 @@ window.addEventListener('DOMContentLoaded', function () {
             //     document.getElementById("container").style.backgroundColor = "#ecc6bf";
             //     break
         }
+        
+        
 
 
+        
 
+        // $(document).ready(function() {
+        //     var bodyOffset = $('body').offset();
+        //     $(window).scroll(function() {
+        //         if ($(document).scrollTop() > bodyOffset.top) {
+        //             $('#splash').addClass('scroll');
+        //             $('.scroll').get(11600).play();
+        //         } else {
+        //             $('.scroll').get(11600).pause();
+        //             $('#splash').removeClass('scroll');
+        //         }
+        //     });
+        // });
 
+//         window.addEventListener('load', videoScroll);
+//         window.addEventListener('scroll', videoScroll);
 
+//         function videoScroll() {
+
+//         if ( document.querySelectorAll('video[autoplay]').length > 0) {
+//             var windowHeight = window.innerHeight,
+//             videoEl = document.querySelectorAll('video[autoplay]');
+
+//     for (var i = 0; i < videoEl.length; i++) {
+
+//       var thisVideoEl = videoEl[i],
+//           videoHeight = thisVideoEl.clientHeight,
+//           videoClientRect = thisVideoEl.getBoundingClientRect().top;
+
+//       if ( videoClientRect <= ( (windowHeight) - (videoHeight*.5) ) && videoClientRect >= ( 0 - ( videoHeight*.5 ) ) ) {
+//         thisVideoEl.play();
+//       } else {
+//         thisVideoEl.pause();
+//       }
+
+//     }
+//   }
+
+// }
+        
+
+// var _video = document.querySelector('.video');
+
+// function isScrolledIntoView( element ) {
+//     var elementTop    = element.getBoundingClientRect().top,
+//         elementBottom = element.getBoundingClientRect().bottom;
+
+//     return elementTop >= 0 && elementBottom <= window.innerHeight;
+// }
+
+// window.addEventListener("scroll", function(){
+//   if (isScrolledIntoView(_video)) {
+//     _video.play();
+//   }
+//   else {
+//     _video.pause()
+//   }
+// })
         
         
 
@@ -101,7 +159,63 @@ window.addEventListener('DOMContentLoaded', function () {
             // }
         }
 
+        
+
     });
+
+    var swiper02 = new Swiper("#main_slide", {
+        // freemode: true,
+        // slidesPerView: 5,
+        // spaceBetween: 100,
+        // autoplay : {
+        //     delay : 0.5,
+        //     pauseOnMouseEnter: true,
+        //     disableOnInteraction: false
+        // },
+        // speed : 11000,
+        // loop: true,
+        // initialSlide: 1,
+        
+        loop: true,
+        freeMode: true,
+        spaceBetween: 0,
+        // grabCursor: true,
+        slidesPerView: 5,
+        loop: true,
+        autoplay: {
+          delay: 1,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false
+        },
+        freeMode: true,
+        speed: 11000,
+        freeModeMomentum: false,
+        initialSlide: 0,
+        watchOverflow:true,
+        
+    });
+    
+    // var mySwiper = document.querySelector('#main_slide').swiper
+
+    // $("#main_slide").mouseenter(function() {
+    //   mySwiper.autoplay.stop();
+    //   console.log('slider stopped');
+    // });
+  
+    // $("#main_slide").mouseleave(function() {
+    //   mySwiper.autoplay.start();
+    //   console.log('slider started again');
+    // });
+  
+    // $("#main_slide").each(function(elem, target){
+    //     var swp = target.swiper;
+    //     $(this).hover(function() {
+    //         swp.autoplay.stop();
+    //     }, function() {
+    //         swp.autoplay.start();
+    //     });
+    // });
+
 
 
 })
